6158548c55c0a4b53f534870b19c1cc49e0a018b
tinamanga <christinamanga28@gmail.com>
2025-03-15 00:11:34 +0300

    building a functioning to-do list application

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.addEventListener('submit', (event)=>{
+    event.preventDefault();
+
+  // Get the value of form input with id new-task-description
+    let taskInput=document.getElementById("new-task-description").value;
+   console.log("Task: "+taskInput);
+
+  //   // Add/display the value to the My Todos List
+    let taskList=document.getElementById("task"); //<ul id="task></ul>"
+    var li=document.createElement("li"); // <li></li>
+   li.appendChild(document.createTextNode(taskInput));
+    taskList.appendChild(li);
+
+  })
 });
